7cfee5fb8f86b3b891076190f632c69beec74896
rikibaybay <kolyaokon00.06.18@gmail.com>
2022-04-09 22:32:33 +0900

    Initial commit

---
+++ src/teatAnimal.java
@@ -0,0 +1,12 @@
+public class teatAnimal {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog doge = new Dog();
+        doge.run(300);
+        cat.run(300);
+        doge.swim(5);
+        cat.swim(5);
+        doge.jump(1.5f);
+        cat.jump(1.5f);
+    }
+}
